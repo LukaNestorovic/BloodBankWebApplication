6a787775dc3b88d48ae187f91615c3d55379ad1b
r1st1c <risticbr021@gmail.com>
2022-10-31 19:12:16 +0100

    added jpa dependency and app properties

--- centar-za-transfuziju-krvi/build.gradle.kts
+++ centar-za-transfuziju-krvi/build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.data:spring-data-jpa:2.7.5")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
