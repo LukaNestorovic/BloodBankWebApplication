952629571142dac62acc54e03e1c2391db6b2ce7
LukaNestorovic <lukanestorovic41@gmail.com>
2022-11-01 21:46:47 +0100

    Models for Center and Registered users

---
+++ centar-za-transfuziju-krvi/src/main/kotlin/com/isa/centarzatransfuzijukrvi/model/Center.kt
@@ -0,0 +1,4 @@
+package com.isa.centarzatransfuzijukrvi.model
+
+data class Center(val name: String, val adress: String, val description: String, val rating: Double) {
+}
---
+++ centar-za-transfuziju-krvi/src/main/kotlin/com/isa/centarzatransfuzijukrvi/model/RegisteredUser.kt
@@ -0,0 +1,6 @@
+package com.isa.centarzatransfuzijukrvi.model
+
+data class RegisteredUser(val id: Int, val name: String, val surname: String, val email: String, val password: String, val adress: String, val city: String, val country: String,
+                          val phone: String, val jmbg: String, val sex: String, val occupation: String, val information: String) {
+
+}
