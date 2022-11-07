977eddc25d79419dce52a6a4858fb1db0082610c
r1st1c <risticbr021@gmail.com>
2022-11-07 12:19:44 +0100

    additional commit

--- centar-za-transfuziju-krvi/src/main/kotlin/com/isa/centarzatransfuzijukrvi/controller/RegisteredUserController.kt
+++ centar-za-transfuziju-krvi/src/main/kotlin/com/isa/centarzatransfuzijukrvi/controller/RegisteredUserController.kt
@@ -5,16 +5,15 @@ import com.isa.centarzatransfuzijukrvi.service.RegisteredUserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@CrossOrigin(origins=["*"])
 class RegisteredUserController(val registeredUserService: RegisteredUserService) {
 
-    @PostMapping(path = ["/api/registration"], consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(path = ["/api/register"], consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun createRegisteredUser(@RequestBody registeredUser: RegisteredUser) : ResponseEntity<RegisteredUser>{
+        println("email:" + registeredUser.email)
         val registeredUser: RegisteredUser = RegisteredUser(registeredUser.id, registeredUser.name, registeredUser.surname, registeredUser.email,
             registeredUser.password, registeredUser.address, registeredUser.city, registeredUser.country, registeredUser.phone, registeredUser.jmbg,
             registeredUser.gender, registeredUser.occupation, registeredUser.information)
