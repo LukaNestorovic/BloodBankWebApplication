1a708f7eb4649622b1733b0ff75dc330fc522bce
r1st1c <risticbr021@gmail.com>
2022-11-07 15:39:24 +0100

    removed redundant constructor

--- centar-za-transfuziju-krvi/src/main/kotlin/com/isa/centarzatransfuzijukrvi/controller/RegisteredUserController.kt
+++ centar-za-transfuziju-krvi/src/main/kotlin/com/isa/centarzatransfuzijukrvi/controller/RegisteredUserController.kt
@@ -15,13 +15,7 @@ class RegisteredUserController(val registeredUserService: RegisteredUserService)
 
     @PostMapping(path = ["/register"], consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun createRegisteredUser(@RequestBody registeredUser: RegisteredUser) : ResponseEntity<RegisteredUser>{
-        val registeredUser: RegisteredUser = RegisteredUser(registeredUser.id, registeredUser.name, registeredUser.surname, registeredUser.email,
-            registeredUser.password, registeredUser.address, registeredUser.city, registeredUser.country, registeredUser.phone, registeredUser.jmbg,
-            registeredUser.gender, registeredUser.occupation, registeredUser.information)
-
         val newRegisteredUser: RegisteredUser = registeredUserService.create(registeredUser)
-
-
         return ResponseEntity(newRegisteredUser, HttpStatus.CREATED)
     }
 }
