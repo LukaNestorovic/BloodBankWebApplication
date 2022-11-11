bacbbb1b1c6dfceef94c3e5c727916503c507e9e
r1st1c <risticbr021@gmail.com>
2022-11-11 10:54:44 +0100

    added possibility of searching and filtering centers

--- react-typescript-api-call/src/services/CenterService.tsx
+++ react-typescript-api-call/src/services/CenterService.tsx
@@ -3,8 +3,11 @@ import axios from "axios";
 const CENTER_API_BASE_URL = "http://localhost:8080/api/";
 
 class CenterService {
-    getCenters() {
+    getCentersGlobal() {
         return axios.get(CENTER_API_BASE_URL + "global-centers");
+    }
+    getCentersGlobalFilter(data: any){
+        return axios.post(CENTER_API_BASE_URL + "global-centers",data)
     }
 }
 
