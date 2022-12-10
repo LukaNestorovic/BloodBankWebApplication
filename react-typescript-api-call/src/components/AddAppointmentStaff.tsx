ff483d08f74c36944cb7b13bbea9f904e05f35ce
r1st1c <risticbr021@gmail.com>
2022-12-10 16:22:54 +0100

    front for adding new appointment

--- react-typescript-api-call/src/components/AddAppointmentStaff.tsx
+++ react-typescript-api-call/src/components/AddAppointmentStaff.tsx
@@ -1,13 +1,107 @@
-import { Container, Typography } from "@mui/material";
+import { Button, Container, FormControl, InputLabel, MenuItem, Select, Stack, TextField, Typography } from "@mui/material";
+import React from "react";
+import { useState } from "react";
+import { string } from "yup";
+import CenterService from "../services/CenterService";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 
-export default function AddAppointmentStaff(){
+
+export default function AddAppointmentStaff() {
+    var i = 0;
+    interface Centers {
+        center: {
+            name: string
+            address: string
+            description: string
+            rating: number
+        }[]
+    }
 
 
-    return(
-        <Container>
-            <Typography>
-                LOL
-            </Typography>
-        </Container>
+    const [centers, setCenters] = useState<Centers["center"]>();
+
+    const [appointment, setAppointment] = useState({
+        centerName: "",
+        date: ""
+    })
+
+    const handleChange = (e: any) => {
+        setAppointment({
+            ...appointment,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    // const [centerName, setCenterName] = useState("Select");
+
+    // const [date, setDate] = useState(new Date());
+
+    // const handleCenterChange = (event: any) => {
+    //     setCenterName(event.target.value)
+    // }
+
+    // const handleDateChange = (event: any) => {
+    //     setDate(event.target.value)
+    // }
+
+    React.useEffect(() => {
+        CenterService.getCenters().
+            then((response) => {
+                console.log(response.data);
+                setCenters(response.data)
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    React.useEffect(() => {
+        console.log(appointment.date)
+    }, [appointment.date, appointment.centerName]);
+
+    return (
+        <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+        >
+            <h1 style={{ alignSelf: 'center' }}>Scheduling new appointment</h1>
+            <FormControl>
+                <InputLabel id="demo-simple-select-label">Center</InputLabel>
+                <Select
+                    style={{ minWidth: 300 }}
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={appointment.centerName}
+                    label="center"
+                    onChange={handleChange}
+                    name="centerName"
+                >
+                    {centers?.map(entry => (
+                        <MenuItem value={entry.name} key={i++}>
+                            {entry.name}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateTimePicker
+                    views={["year","month","day","hours"]}
+                    disablePast
+                    ampm={false}
+                    renderInput={(props) => <TextField {...props} />}
+                    label="Date and Time"
+                    value={appointment.date}
+                    onChange={(newValue) => setAppointment({
+                        ...appointment,
+                        date: newValue
+                    })}
+                />
+            </LocalizationProvider>
+            <Button variant="contained" style={{ width: 200, alignSelf: 'center' }}>Save</Button>
+        </Stack>
     );
 }
