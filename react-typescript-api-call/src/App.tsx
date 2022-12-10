2323a116654156310fec2f40b5e0e179e66d8da6
r1st1c <risticbr021@gmail.com>
2022-12-10 12:38:10 +0100

    added backend for adding appointment and frontend frame
    (cherry picked from commit 98be67487b920d001ffda7b17c6b007add42e0e1)

--- react-typescript-api-call/src/App.tsx
+++ react-typescript-api-call/src/App.tsx
@@ -8,6 +8,7 @@ import UpdateProfile from "./components/UpdateProfile"
 import GlobalCenters from "./components/GlobalCenters"
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Questionnaire from "./components/Questionnaire";
+import AddAppointmentStaff from './components/AddAppointmentStaff';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
             <Route path="/global-centers" element={<GlobalCenters />} />
             <Route path="/centers" element={<Centers />}/>
             <Route path="/question" element={<Questionnaire/>}/>
+            <Route path="/appointment/staff" element={<AddAppointmentStaff/>}/>
           </Routes>
         </BrowserRouter>
       </>
