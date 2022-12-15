57aab8e3a04bb868adcef98294a8f48034affa23
JovanSavkovic <savkovic.jovan@protonmail.com>
2022-12-15 13:21:43 +0100

    cherry pick email

--- centar-za-transfuziju-krvi/build.gradle.kts
+++ centar-za-transfuziju-krvi/build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-jdbc")
-
+	implementation("org.springframework.boot:spring-boot-starter-mail")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
