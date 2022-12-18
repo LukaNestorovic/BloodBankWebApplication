c4cdf88c68a38cc09240a35e6f4ac97a9dd8ddb3
r1st1c <risticbr021@gmail.com>
2022-12-18 12:37:03 +0100

    enableasync

--- centar-za-transfuziju-krvi/src/main/kotlin/com/isa/centarzatransfuzijukrvi/CentarZaTransfuzijuKrviApplication.kt
+++ centar-za-transfuziju-krvi/src/main/kotlin/com/isa/centarzatransfuzijukrvi/CentarZaTransfuzijuKrviApplication.kt
@@ -2,8 +2,10 @@ package com.isa.centarzatransfuzijukrvi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableAsync
 class CentarZaTransfuzijuKrviApplication
 
 fun main(args: Array<String>) {
